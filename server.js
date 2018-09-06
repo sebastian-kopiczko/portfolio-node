e301389ec57163ec9db442ea0e1ac10c6f2db29e
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-09-06 13:04:46 +0200

    ejs variables fixes

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ const port = 5000;
 app.listen(port, function(){
   console.log('server running');
 });
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index', {
-  pageTitle: 'Sebastian Kopiczko - junior developer',
-  fullName: 'Sebastian Kopiczko'
-}));
+app.get('/', (req, res) => res.render('index', { config: config }));
 app.get('/about', (req, res) => res.render('about'));
 app.get('/projects', (req, res) => res.render('projects'));
 app.get('/contact', (req, res) => res.render('contact'));
