9ed53c1753cf03e5b8179acce3e26e5a408437be
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-09-06 16:17:20 +0200

    setup i18n to handle translations, crate buttons to switch between en/pol

--- public/js/code.js
+++ public/js/code.js
@@ -1,25 +1,52 @@
 const UISelectors = {
-  menu: document.querySelector("#menu-overlay"),
-  menuBtn: document.querySelector("#menu-btn"),
-  menuTrigger: document.querySelector(".menu__trigger")
+    menu: document.querySelector("#menu-overlay"),
+    menuBtn: document.querySelector("#menu-btn"),
+    menuTrigger: document.querySelector(".menu__trigger"),
+    changeLang: document.querySelector("#change-lang")
 };
+
+UISelectors.changeLang.addEventListener('click', (e) => {
+    if (e.target.dataset.lang === 'pl') {
+        setLanguage(e.target.dataset.lang);
+    } else {
+        setLanguage(e.target.dataset.lang);
+    }
+})
+const setLanguage = lang => {
+    fetch('/set', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            language: lang
+        })
+    })
+    .then(res => {
+    if(res.ok){
+        window.location.reload(true);
+    }
+
+    });
+}
 UISelectors.menuBtn.addEventListener('click', (e) => {
-  if (e.target.classList.contains('menu__trigger')) {
-      UISelectors.menuBtn.classList.toggle('button-active');
-      UISelectors.menu.classList.toggle('active');
-  };
+    if (e.target.classList.contains('menu__trigger')) {
+        UISelectors.menuBtn.classList.toggle('button-active');
+        UISelectors.menu.classList.toggle('active');
+    };
 });
 const inputs = document.querySelectorAll('.form__input');
 inputs.forEach((input) => {
-  if (input.value) {
-      e.target.parentElement.lastElementChild.classList.add('form__label--active')
-  };
-  input.addEventListener('focus', (e) => {
-      e.target.parentElement.lastElementChild.classList.add('form__label--active');
-  });
-  input.addEventListener('focusout', (e) => {
-      if (!e.target.value) {
-          e.target.parentElement.lastElementChild.classList.remove('form__label--active');
-      }
-  });
+    if (input.value) {
+        e.target.parentElement.lastElementChild.classList.add('form__label--active')
+    };
+    input.addEventListener('focus', (e) => {
+        e.target.parentElement.lastElementChild.classList.add('form__label--active');
+    });
+    input.addEventListener('focusout', (e) => {
+        if (!e.target.value) {
+            e.target.parentElement.lastElementChild.classList.remove('form__label--active');
+        }
+    });
 });
--- server.js
+++ server.js
@@ -1,14 +1,56 @@
 const express = require('express');
+const i18n = require('i18n-2');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 5000;
-app.listen(port, function(){
-  console.log('server running');
-});
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.render('index'));
+i18n.expressBind(app, {
+  query: true,
+  locales: {
+    "en": {
+      "about": "About",
+      "projects": "Projects",
+      "contact": "Contact",
+      "test": "english"
+    },
+    "pl": {
+      "about": "O mnie",
+      "projects": "Projekty",
+      "contact": "Kontakt",
+      "test": "polski"
+    }
+  },
+  cookieName: 'locale'
+});
+let globalLang = 'pl';
+app.use((req, res, next) => {
+  req.i18n.setLocale(globalLang);
+  next();
+});
+
+app.post('/set', (req, res) => {
+  const lang = req.body.language;
+  if(lang !== globalLang){
+    globalLang = lang;
+    req.i18n.setLocale(lang);
+    res.json({ ok: true });
+  } else { return }
+});
+
+app.get('/', (req, res) => {res.render('index', {
+  title: req.i18n.__("test")}
+)});
 app.get('/about', (req, res) => res.render('about'));
 app.get('/projects', (req, res) => res.render('projects'));
 app.get('/contact', (req, res) => res.render('contact'));
 app.get('*', (req, res) => res.render('404'));
+
+app.listen(port, () => {
+  console.log('server running');
+});
