609ecf25aea7f5c09bfa2a96e0b6db2688324b25
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-09-06 12:15:11 +0200

    add script code

--- public/js/code.js
+++ public/js/code.js
@@ -1,1 +1,26 @@
-console.log(111);
+const UISelectors = {
+  menu: document.querySelector("#menu-overlay"),
+  menuBtn: document.querySelector("#menu-btn"),
+  menuTrigger: document.querySelector(".menu__trigger")
+};
+UISelectors.menuBtn.addEventListener('click', (e) => {
+  if (e.target.classList.contains('menu__trigger')) {
+      console.log('menu triggered');
+      UISelectors.menuBtn.classList.toggle('button-active');
+      UISelectors.menu.classList.toggle('active');
+  };
+});
+const inputs = document.querySelectorAll('.form__input');
+inputs.forEach((input) => {
+  if (input.value) {
+      e.target.parentElement.lastElementChild.classList.add('form__label--active')
+  };
+  input.addEventListener('focus', (e) => {
+      e.target.parentElement.lastElementChild.classList.add('form__label--active');
+  });
+  input.addEventListener('focusout', (e) => {
+      if (!e.target.value) {
+          e.target.parentElement.lastElementChild.classList.remove('form__label--active');
+      }
+  });
+});
