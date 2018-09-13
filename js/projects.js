4305871cb8a72c06f586367ad4191808bd061c1f
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-09-13 16:17:03 +0200

    update project list

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,3 @@
-const translation = require('./translations');
 module.exports = (function(){
   const projects = [
     {
@@ -6,7 +5,7 @@ module.exports = (function(){
       title: 'CV',
       desc: 'Webowe CV',
       tags: 'css grid, html, webpack',
-      live: 'https://sebastiankopiczko.pl/resume/',
+      live: 'https://s-kopiczko.usermd.net/resume',
       github: 'https://github.com/sebastian-kopiczko/resume'
     },
     {
@@ -30,23 +29,15 @@ module.exports = (function(){
       title: 'weather-app',
       desc: 'Aplikacja sprawdzająca aktualną pogodę (Dark Sky API) dla podanej lokalizacji (Google API).',
       tags: 'javascript, webpack, API, local storage, materialize css',
-      live: 'https://s-kopiczko.usermd.net/weather-app/',
+      live: 'https://s-kopiczko.usermd.net/app/weather-app/',
       github: 'https://github.com/sebastian-kopiczko/weather-app'
-    },
-    {
-      name: 'notes-app',
-      title: 'notes-app',
-      desc: 'Prosta aplikacja pozwalająca na dodawanie, edycję oraz usuwanie notatek.',
-      tags: 'javascript, webpack, local storage, picnic css',
-      live: 'https://s-kopiczko.usermd.net/notes-app/',
-      github: 'https://github.com/sebastian-kopiczko/notes-manager'
     },
     {
       name: 'restaurant-psd',
       title: 'restaurant-psd',
       desc: 'One page zakodowany na podstawie darmowego szablonu PSD.',
       tags: 'html, scss, bs4, webpack',
-      live: 'http://s-kopiczko.usermd.net/psd-restaurant/',
+      live: 'http://s-kopiczko.usermd.net/psd/psd-restaurant/',
       github: 'https://github.com/sebastian-kopiczko/restaurant-website'
     },
     {
@@ -54,33 +45,43 @@ module.exports = (function(){
       title: 'poland-psd',
       desc: 'Strona zakodowana na podstawie darmowego szablonu PSD.',
       tags: 'html, scss, gulp',
-      live: 'http://s-kopiczko.usermd.net/psd-poland/',
+      live: 'http://s-kopiczko.usermd.net/psd/psd-poland/',
       github: 'https://github.com/sebastian-kopiczko/Fall-in-love-with-Poland---coding-PSD'
     },
     {
-      name: 'youtube-app',
-      title: 'youtube-app',
-      desc: 'W BUDOWIE - Aplikacja pozwalająca na pobieranie danych o kanałach oraz wyszukiwanie filmów z serwisu YT.',
-      tags: 'materialize css, javascript',
-      // live: 'https://s-kopiczko.usermd.net/youtube-app/',
-      github: 'https://github.com/sebastian-kopiczko/youtube-api'
-    },
-    {
-      name: 'tips-calculator',
-      title: 'tips-calculator',
-      desc: 'W BUDOWIE - Prosty kalkulator obliczający wysokość napiwku. Projekt wykonany na zaliczenie przedmiotu na uczelni.',
-      tags: 'html, ionic, typescript',
-      // live: 'http://s-kopiczko.usermd.net/tip-calculator/',
-      github: 'https://github.com/sebastian-kopiczko/tip-calculator-ionic'
+      name: 'notes-app',
+      title: 'notes-app',
+      desc: 'Prosta aplikacja pozwalająca na dodawanie, edycję oraz usuwanie notatek.',
+      tags: 'javascript, webpack, local storage, picnic css',
+      live: 'https://s-kopiczko.usermd.net/app/notes-app/',
+      github: 'https://github.com/sebastian-kopiczko/notes-manager'
     },
     {
       name: 'shopping-list',
       title: 'shopping-list',
       desc: 'Prosta aplikacją służąca do tworzenia listy zakupów.',
       tags: 'bulma css, javascript',
-      live: 'https://s-kopiczko.usermd.net/shopping-list/',
+      live: 'https://s-kopiczko.usermd.net/app/shopping-list/',
       github: 'https://github.com/sebastian-kopiczko/shopping-list'
-    } 
+    },
+    {
+      name: 'youtube-app',
+      title: 'youtube-app',
+      desc: 'W BUDOWIE - Aplikacja pozwalająca na pobieranie danych o kanałach oraz wyszukiwanie filmów z serwisu YT.',
+      tags: 'materialize css, javascript',
+      // live: 'https://s-kopiczko.usermd.net/app/youtube-app/',
+      github: 'https://github.com/sebastian-kopiczko/youtube-api',
+      inProgress: true
+    },
+    {
+      name: 'tips-calculator',
+      title: 'tips-calculator',
+      desc: 'W BUDOWIE - Prosty kalkulator obliczający wysokość napiwku. Projekt wykonany na zaliczenie przedmiotu na uczelni.',
+      tags: 'html, ionic, typescript',
+      // live: 'http://s-kopiczko.usermd.net/app/tip-calculator/',
+      github: 'https://github.com/sebastian-kopiczko/tip-calculator-ionic',
+      inProgress: true
+    }
   ];
   return projects;
 })();
